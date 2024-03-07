ef2f7446b5afe50cb3cb28590aca93e8e04913b9
DeeOneCoder <101057409+DeeOneCoder@users.noreply.github.com>
2024-03-07 12:16:35 +0100

    This is the first commit from Git master class

---
+++ Marvelous.java
@@ -0,0 +1,7 @@
+
+public class Marvelous{
+	public static void main(String[] args){
+		System.out.println("Hello, I am new in Java");
+		System.out.println("Testing staging in Git Repository");
+	}
+}
